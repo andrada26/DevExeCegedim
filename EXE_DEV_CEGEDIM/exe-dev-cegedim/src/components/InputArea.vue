273b7186b667cfaff272a33f7eccfae2ab24befd
andrada26 <romanandrada26@yahoo.com>
2023-11-23 15:18:06 +0200

    Update 2 >>D incomplet

--- EXE_DEV_CEGEDIM/exe-dev-cegedim/src/components/InputArea.vue
+++ EXE_DEV_CEGEDIM/exe-dev-cegedim/src/components/InputArea.vue
@@ -1,28 +1,48 @@
 
 <template>
-  <div class="userinput">
+  <div class="userInput">
     <div class="divInputStyle"><input class="inputStyle" type="text" v-model="inputText"
         placeholder="Introduceti numerele aici..." /></div>
-    <div class="divButtonStyle"><button class="buttonStyle" @click="handleButtonClick">Calculate</button></div>
+    <div class="divButtonStyle"><button class="buttonStyle" @click="createMax">Calculate</button></div>
   </div>
-  <table v-if="resultMatrix.length > 0">
-    <thead>
-      <tr>
-        <th class="tittleStyle">Număr</th>
-        <th class="tittleStyle">Prima Cifră</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(row, index) in resultMatrix" :key="index">
-        <td>{{ row[0] }}</td>
-        <td>{{ row[1] }}</td>
-      </tr>
-    </tbody>
-  </table>
-  <p v-if="resultMaxMatrix.length > 0">
-    <strong>Maximele din clase sunt :</strong> {{ resultMaxMatrix }}
-    <br>
-  </p>
+  <div class="container">
+    <div class="tableStyle">
+      <table v-if="bMatrix.length > 0">
+        <thead>
+          <tr>
+            <th class="tittleStyle">Număr</th>
+            <th class="tittleStyle">Prima Cifră</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, index) in bMatrix" :key="index">
+            <td>{{ row[0] }}</td>
+            <td>{{ row[1] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="maxStyle" v-if="bMaxMatrix.length > 0">
+      <p v-if="bMaxMatrix.length > 0">
+        <strong>Maximele din clase sunt :</strong> {{ bMaxMatrix }}
+        <br>
+      </p>
+      <p v-if="charResult.length > 0">
+        <strong>Modulo :</strong> {{ charResult }}
+        <br>
+      </p>
+    </div>
+  </div>
+  <div class="matrixStyle">
+    <table class="table2Style" v-if="cMatrix.length > 0">
+      <tbody>
+        <tr class="rowStyle" v-for="(row, rowIndex) in 4" :key="rowIndex">
+            <td class="colStyle" v-for="(col, colIndex) in 4" :key="colIndex">{{ cMatrix[rowIndex * 4 + colIndex] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
@@ -30,19 +50,20 @@ import { ref } from 'vue';
 
 
 const inputText = ref('');
-const resultMatrix = ref([]);
-const resultMaxMatrix = ref([]);
+const bMatrix = ref([]);
+const bMaxMatrix = [];
+const cMatrix = ref([]);
+const charResult = [];
 
-const handleButtonClick = () => {
-  resultMatrix.value = []; // Remove last values  
-  resultMaxMatrix.value = [];
+
+const createMax = () => {
 
   const numbers = inputText.value.split(",");
   let smallestInteger = Number.MIN_SAFE_INTEGER;
 
   for (const number of numbers) {
     const firstDigit = Number(number[0]);
-    resultMatrix.value.push([Number(number), firstDigit]);
+    bMatrix.value.push([Number(number), firstDigit]);
   }
 
 
@@ -50,21 +71,51 @@ const handleButtonClick = () => {
     smallestInteger = Number.MIN_SAFE_INTEGER;
     let foundclass = 0;
 
-    for (let i = 0; i < resultMatrix.value.length; i++) {
+    for (let i = 0; i < bMatrix.value.length; i++) {
 
-      if (resultMatrix.value[i][0] > smallestInteger && k == resultMatrix.value[i][1]) {
-        console.log(`Verificam: ${smallestInteger} cu ${resultMatrix.value[i][0]}`);
-        smallestInteger = resultMatrix.value[i][0];
+      if (bMatrix.value[i][0] > smallestInteger && k == bMatrix.value[i][1]) {
+        console.log(`Verificam: ${smallestInteger} cu ${bMatrix.value[i][0]}`);
+        smallestInteger = bMatrix.value[i][0];
         foundclass = 1;
       }
 
     }
-    if(foundclass == 1)
-    resultMaxMatrix.value.push(smallestInteger);
+    if (foundclass == 1)
+      bMaxMatrix.push(smallestInteger);
 
   }
+  toChar(bMaxMatrix);
+
+};
+const toChar = (numbers) => {
+
+  for (const number of numbers) {
+    let numericValue = Number(number);
+    //aducem numărul în intervalul [0, 25]
+    numericValue = ((numericValue % 26) + 26) % 26;
 
 
+    const correspondingLetter = String.fromCharCode("A".charCodeAt(0) + numericValue);
+
+    console.log(correspondingLetter);
+    charResult.push(correspondingLetter);
+
+  }
+  createCMatrix(charResult);
+};
+
+const createCMatrix = (charResult) => {
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const randomIndex = Math.floor(Math.random() * charResult.length);
+      console.log(`S-a ales indexul random: ${randomIndex} cu valoarea  ${charResult[randomIndex]}`);
+      cMatrix.value.push(charResult[randomIndex]);
+
+    }
+
+  }
+  return cMatrix;
 };
 </script>
 
@@ -76,12 +127,20 @@ h3 {
 }
 
 .divInputStyle {
-  display: inline-block;
+  height: 100%;
+  width: 100%;
+}
+
+.userInput {
+  margin-bottom: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .inputStyle {
   height: 20px;
-  width: 600px;
+  width: 100%;
 }
 
 .divButtonStyle {
@@ -92,7 +151,44 @@ h3 {
   height: 27px;
 }
 
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tableStyle {
+  display: inline-block;
+}
+
 .tittleStyle {
   padding-left: 10px;
+}
+
+.maxStyle {
+  display: inline-block;
+  border: solid;
+  border-radius: 2px;
+  border-color: blue;
+  border-width: 2px;
+}
+
+.matrixStyle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.table2Style{
+  width: 100%;
+}
+.rowStyle{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
+.colStyle{
+  margin-top: 2rem;
+  margin-bottom: 2rem;
 }
 </style>
